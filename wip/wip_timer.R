78da1612e2f021647cbf7c3c3206e4310fdbf711
dkidney <darrenkidney@yahoo.co.uk>
2019-06-04 23:18:42 +0100

    update to wip timer

--- wip/wip_timer.R
+++ wip/wip_timer.R
@@ -1,19 +1,60 @@
 
 
-f = function(secs = 2){
-    message1 = "-  checking something ..."
-    nchar1 = nchar(message1)
-    cat(message1, "\r")
-    
-    Sys.sleep(secs)
-    
-    message2 = " finished :)"
-    message2 = cli::col_blue(message2)
-    message2 = paste(message2, cli::col_cyan("(time)"))
-    nchar2 = nchar(message2)
-    message2 = stringr::str_pad(message2, width = max(nchar1, nchar2), side = "right", pad = " ")
-    cli::cat_bullet(message2, bullet = "tick", bullet_col = "green")
+f = function(message = "checking something"){
+    msg_1 = paste("- ", message, "...")
+    nc_1 = nchar(msg_1)
+    cat(msg_1, "\r")
+
+    start_time = Sys.time()
+    Sys.sleep(2)
+    finish_time = Sys.time()
+    dt = prettyunits::pretty_dt(difftime(finish_time, start_time))
+    dt = str_c("(", dt, ")")
+
+    msg_2 = message
+    nc_2 = nchar(msg_2) + nchar(dt) + 3
+    msg_2 = cli::col_silver(msg_2)
+    msg_2 = paste(msg_2, cli::col_cyan(dt))
+    msg_2 = stringr::str_pad(msg_2, width = max(nc_1, nc_2), side = "right", pad = " ")
+    cli::cat_bullet(msg_2, bullet = "tick", bullet_col = "green")
 }
 
 f()
+
+
+my_function = function(secs){
+    Sys.sleep(secs)
+    "hello"
+}
+
+timer = function(.f, message = "checking something"){
+    function(...){
+        msg_1 = paste("- ", message, "...")
+        nc_1 = msg_1 %>% str_length
+        cat(msg_1, "\r")
+        start_time = Sys.time()
+        result = .f(...)
+        finish_time = Sys.time()
+        dt = finish_time %>%
+            difftime(start_time) %>%
+            prettyunits::pretty_dt() %>%
+            str_c("(", ., ")")
+        msg_2 = message
+        nc_2 = nchar(msg_2) + nchar(dt) + 3
+        msg_2 = cli::col_silver(msg_2)
+        msg_2 = paste0(" ", msg_2, " ", cli::col_cyan(dt))
+        msg_2 = stringr::str_pad(msg_2, width = max(nc_1, nc_2), side = "right", pad = " ")
+        cli::cat_bullet(msg_2, bullet = "tick", bullet_col = "green")
+        result
+    }
+}
+
+g = function(){
+    x = timer(my_function, "asdfasdf")(3)
+    y = timer(my_function, "1212121212")(1)
+    list(x, y)
+}
+
+g()
+
 
