8c385c5d94acd297a0c24aa111387c7233df30d4
dkidney <darrenkidney@yahoo.co.uk>
2019-06-19 09:53:40 +0100

    added has_internet to wip_functions

--- R/utils.R
+++ R/utils.R
@@ -1,11 +1,4 @@
 
-has_internet <- function() {
-  content <- try(suppressWarnings({
-    readLines("http://www.r-project.org", n = 1)
-  }), TRUE)
-  !inherits(content, "try-error")
-}
-
 # -------------------------------------------------------------------------------------- #
 
 #' @title Convert colours to hexadecimal format
