b5079511e23c52aca882e1f340a4b447daee7cb8
darrenkidney <darren.kidney@nielsen.com>
2019-06-21 17:54:46 +0100

    improvement to wip tz_set

--- wip/wip_tz_functions.R
+++ wip/wip_tz_functions.R
@@ -3,40 +3,46 @@
 # OlsonNames()
 
 tz_set = function(x, tz){
-    stopifnot(inherits(x, "POSIXct") && length(x) == 1)
-    as.POSIXct(format(x, tz = tz, usetz = TRUE), tz = tz)
+    stopifnot(inherits(x, c("POSIXct", "POSIXlt")))
+    stopifnot(length(tz) == 1 && tz %in% OlsonNames())
+    as.POSIXct(as.POSIXlt(x, tz = tz))
 }
 
 tz_get = function(x){
-    stopifnot(inherits(x, "POSIXct") && length(x) == 1)
+    stopifnot(inherits(x, c("POSIXct", "POSIXlt")))
     lubridate::tz(x)
 }
 
 
 if(0){
-
-    # lubridate::tz changes tz but not the time
-    x = y = Sys.time()
-    lubridate::tz(y) = "America/New_York"
-    tz_get(x)
-    tz_get(y)
-    x
-    y
-
-    # lubridate::force_tz changes tz but not the time
-    x = y = Sys.time()
-    y = lubridate::force_tz(y, "America/New_York")
-    tz_get(x)
-    tz_get(y)
-    x
-    y
-
-    # set_tz changes tz and time
-    x = y = Sys.time()
-    y = tz_set(x, "America/New_York")
-    tz_get(x)
-    tz_get(y)
-    x
-    y
-
+    
+    library(tidyverse)
+    
+    tz = "America/New_York"
+    (x = Sys.time())
+    
+    tests = tibble(test = list(
+        lubridate::`tz<-`(x, tz),
+        lubridate::force_tz(x, tz),
+        as.POSIXlt(x, tz = tz),
+        as.POSIXct(x, tz = tz),
+        as.POSIXct(format(x, tz = tz, usetz = TRUE), tz = tz),
+        as.POSIXct(as.POSIXlt(x, tz = tz)),
+        tz_set(x, tz)
+    ))
+    
+    tests %>% 
+        mutate(
+            tz    = test %>% map_lgl(~identical(tz_get(.x), !!tz)),
+            class = test %>% map_lgl(~identical(class(.x), class(x))),
+            time  = test %>% map_lgl(~as.numeric(difftime(.x, x)) == 0)
+        )
+    
+    # lubridate::tz and lubridate::force_tz change tz but not the time
+    # as.POSIXlt changes the class
+    # as.POSIXct doesn't do anything...
+    # as.POSIXlt(format(...)) doesn't preserve the time accurately
+    # as.POSIXct(as.POSIXlt(...)) does the job
+    # set_tz is just a wrapper for as.POSIXct(as.POSIXlt(...)) 
+    
 }
