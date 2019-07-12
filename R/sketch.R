1d36b12a58034b963fab436584ea0684e0c00d2b
darrenkidney <darren.kidney@nielsen.com>
2019-07-12 21:28:34 +0100

    added sketch placeholder

---
+++ R/sketch.R
@@ -0,0 +1,19 @@
+
+# @rdname TODO
+# @name sketch
+#' @title TODO
+#' @description TODO
+#' @param x TODO
+# @details TODO
+# @importFrom TODO TODO
+# @export
+# @example inst/examples/examples-sketch.R
+#' @examples
+#' \dontrun{
+#'
+#' # TODO
+#' }
+sketch = function(x){
+    
+}
+    
