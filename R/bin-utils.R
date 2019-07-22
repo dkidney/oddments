0a0e9bef7b8abe219ddc63fdf0afcce705f91d16
dkidney <darrenkidney@yahoo.co.uk>
2019-07-22 20:46:52 +0100

    updated docs and examples for bin utils

--- R/bin-utils.R
+++ R/bin-utils.R
@@ -3,13 +3,14 @@
 #' @title Alternative cut functions
 #' @description
 #' \itemize{
-#'     \item \code{bin()} - is equivalent to \link[base]{cut} but uses alternative
+#'     \item \code{bin()} - wraps \link[base]{cut} but uses alternative
 #'     defaults: \code{include.lowest = TRUE, right = FALSE, dig.lab = 5}
 #'     \item \code{bin_pretty()} - uses \link[base]{pretty} to determine the cut points
 #'     \item \code{bin_quantile()} - uses \link[stats]{quantile} to determine the cut
-#'     points. It works similarly to \link[ggplot2]{cut_number} but if there is
-#'     insufficient information for \code{n} cut points it tries to find a lower value of
-#'     \code{n} that does not produce an error
+#'     points. It works similarly to \link[ggplot2]{cut_number} but uses unique quantiles
+#'     (and is therefore less prone to error than \link[ggplot2]{cut_number}). If class
+#'     other than integer or numeric is supplied then a warning is given and the original
+#'     variable is returned unchanged.
 #' }
 #' @param x numeric vector
 #' @param breaks a numeric vector of cut points or a single number giving the number of
@@ -30,16 +31,17 @@
 #'
 #' df %>% ggplot + geom_bar(aes(bin(x, breaks = 5)))
 #' df %>% ggplot + geom_bar(aes(bin_pretty(x)))
-#' df %>% ggplot + geom_bar(aes(bin_quantile(x, n = 5)))
-#' df %>% ggplot + geom_bar(aes(cut_number(x, n = 5))) # identical
+#' df %>% ggplot + geom_bar(aes(cut_number(x, n = 5))) 
+#' df %>% ggplot + geom_bar(aes(bin_quantile(x, n = 5))) # identical to cut_number
 #'
 #' df %>% ggplot + geom_bar(aes(bin(y, breaks = 5)))
 #' df %>% ggplot + geom_bar(aes(bin_pretty(y)))
-#' df %>% ggplot + geom_bar(aes(bin_quantile(y, n = 5)))
 #' df %>% ggplot + geom_bar(aes(cut_number(y, n = 5))) # throws an error
+#' df %>% ggplot + geom_bar(aes(bin_quantile(y, n = 5))) # calculates fewer bins
 #'
 #' df %>% ggplot + geom_bar(aes(bin(z, breaks = 5)))
 #' df %>% ggplot + geom_bar(aes(bin_pretty(z)))
+#' df %>% ggplot + geom_bar(aes(cut_number(z, n = 5))) # throws an error
 #' df %>% ggplot + geom_bar(aes(bin_quantile(z, n = 5)))
 #' }
 NULL
